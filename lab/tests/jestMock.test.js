6166ad4148490f40c2da379403bf8cf7daff84e0
Angeline <atan@thoughtworks.com>
2019-05-29 04:44:51 +0800

    Unskip test

--- lab/tests/jestMock.test.js
+++ lab/tests/jestMock.test.js
@@ -1,17 +1,17 @@
-const game = require('../game')
-const utilsMock =  require('../utils')
+const game = require("../game");
+const utilsMock = require("../utils");
 
-jest.mock('../utils', () => {
+jest.mock("../utils", () => {
   return {
-    getWinner: jest.fn((p1, p2) => p2),
-  }
-})
+    getWinner: jest.fn((p1, p2) => p2)
+  };
+});
 
-xtest('returns winner', () => {
-  const winner = game('Mary', 'Bob')
-  expect(winner).toBe('Bob')
-  expect(utilsMock.getWinner).toHaveBeenCalledTimes(2)
+test("returns winner", () => {
+  const winner = game("Mary", "Bob");
+  expect(winner).toBe("Bob");
+  expect(utilsMock.getWinner).toHaveBeenCalledTimes(2);
   utilsMock.getWinner.mock.calls.forEach(args => {
-    expect(args).toEqual(['Mary', 'Bob'])
-  })
-})
+    expect(args).toEqual(["Mary", "Bob"]);
+  });
+});
