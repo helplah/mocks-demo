29a445e17d1c98652e4bee7b4ace8c592c57af74
Angeline <atan@thoughtworks.com>
2019-05-29 04:47:19 +0800

    Remove answer for the tests

--- lab/tests/jestMock.test.js
+++ lab/tests/jestMock.test.js
@@ -1,17 +1,7 @@
-const game = require('../game')
-const utilsMock =  require('../utils')
+const game = require("../game");
+const utilsMock = require("../utils");
 
-jest.mock('../utils', () => {
-  return {
-    getWinner: jest.fn((p1, p2) => p2),
-  }
-})
-
-xtest('returns winner', () => {
-  const winner = game('Mary', 'Bob')
-  expect(winner).toBe('Bob')
-  expect(utilsMock.getWinner).toHaveBeenCalledTimes(2)
-  utilsMock.getWinner.mock.calls.forEach(args => {
-    expect(args).toEqual(['Mary', 'Bob'])
-  })
-})
+// Write the test for game but mock the getWinner() with jest.mock
+test("returns winner", () => {
+  
+});
--- lab/tests/spyOn.test.js
+++ lab/tests/spyOn.test.js
@@ -1,16 +1,8 @@
 const game = require("../game");
 const utils = require("../utils");
 
+// Write the test for game but mock the getWinner() with jest.spyOn
 test("returns winner", () => {
-  const mockedGetWinner = jest.spyOn(utils, "getWinner");
-  mockedGetWinner.mockImplementation((p1, p2) => p2);
-
-  const winner = game("Mary", "Bob");
-  expect(winner).toBe("Bob");
-  expect(mockedGetWinner).toHaveBeenCalledTimes(2);
-  utils.getWinner.mock.calls.forEach(args => {
-    expect(args).toEqual(["Mary", "Bob"]);
-  });
-
-  mockedGetWinner.mockRestore();
+  
+ 
 });
